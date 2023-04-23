9152f18ea6f06bb0ae81dce40c0c3541f9e94426
Leyla <git@jan-leila.com>
2023-04-23 18:03:09 -0500

    added prefetching for random blocks

--- src/main/java/run/Run.java
+++ src/main/java/run/Run.java
@@ -11,9 +11,13 @@ import server.ServerClient;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Run<T extends RandomType> {
     private final RunStart runStart;
@@ -21,6 +25,9 @@ public class Run<T extends RandomType> {
     private EventTokenLogger eventTokenLogger;
     private String runKey;
     private RandomSource<T> activeSource;
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private TokenResponse<RandomBlock> nextBlock;
 
     public Run(ServerClient serverClient, EventTokenLogger eventTokenLogger, String seed, String runKey) {
         TokenResponse<RunStart> runStart = serverClient.startRun(seed);
@@ -34,6 +41,23 @@ public class Run<T extends RandomType> {
         initRun(serverClient, eventTokenLogger, runStart.token(), savedRandom, runKey);
     }
 
+    private Long getActiveBlockIndex() {
+        return Duration.between(runStart.startTime(), Instant.now()).toMillis() / runStart.blockSize();
+    }
+
+    private TokenResponse<RandomBlock> fetchBlock(Long block) {
+        TokenResponse<RandomBlock> randomBlock = this.serverClient.getRandom(block);
+        this.eventTokenLogger.randomBlock(runKey, randomBlock);
+        return randomBlock;
+    }
+
+    private TokenResponse<RandomBlock> getBlock(Long block) {
+        if (Objects.equals(this.nextBlock.data().block(), block)) {
+            return this.nextBlock;
+        }
+        return fetchBlock(block);
+    }
+
     private void initRun(ServerClient serverClient, EventTokenLogger eventTokenLogger, String runToken, SavedRandom<T> savedRandom, String runKey) {
         this.serverClient = new RunServerClient(serverClient, runToken);
         this.eventTokenLogger = eventTokenLogger;
@@ -41,28 +65,30 @@ public class Run<T extends RandomType> {
         if (savedRandom != null) {
             this.activeSource = new RandomSource<>(savedRandom.block(), savedRandom.calls());
         }
-    }
-
-    private Long getActiveBlockIndex() {
-        return Duration.between(runStart.startTime(), Instant.now()).toMillis() / runStart.blockSize();
+        long initDelay = runStart.blockSize() - (Duration.between(runStart.startTime(), LocalDateTime.now()).toMillis() % runStart.blockSize());
+        scheduler.scheduleAtFixedRate(() -> {
+            long nextBlock = getActiveBlockIndex() + 1;
+            this.nextBlock = fetchBlock(nextBlock);
+        }, initDelay, runStart.blockSize(), TimeUnit.MILLISECONDS);
     }
 
     private RandomSource<T> getActiveBlock() {
         Long activeBlock = this.getActiveBlockIndex();
         while (!Objects.equals(this.activeSource.block.block(), activeBlock)) {
-            TokenResponse<RandomBlock> block = this.serverClient.getRandom(activeBlock);
-            this.eventTokenLogger.randomBlock(runKey, block);
+            TokenResponse<RandomBlock> block = getBlock(activeBlock);
             this.activeSource = new RandomSource<>(block.data(), new HashMap<>());
             activeBlock = this.getActiveBlockIndex();
         }
         return this.activeSource;
     }
 
+    @SuppressWarnings("unused")
     public Random getRandom(T eventType) {
         RandomSource<T> randomSource = this.getActiveBlock();
         return randomSource.getRandom(eventType);
     }
 
+    @SuppressWarnings("unused")
     public TokenResponse<Timebox> timebox(String hash, String cause) {
         return this.serverClient.timeboxRun(hash, cause);
     }
